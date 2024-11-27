80f7f89020d3103a19e219c9610c5c03b360c608
Abhishek Gupta <abhisheksatna001@gmail.com>
2024-11-27 23:07:44 +0530

    jj

--- JavaProject/src/com/nit/test/Test.java
+++ JavaProject/src/com/nit/test/Test.java
@@ -6,15 +6,11 @@ public class Test {
 
 		System.out.println("Abhishek Class ");
 		System.err.println("Suraj Bhaiya Sonam Bhabhi ko mera navaskar krna");
-<<<<<<< HEAD
-		System.out.println("Thik h Bata Bol Denge Tum Tention na lo");
-		System.out.println("Anisha ko patwa do");
-		System.out.println("Anisha ko patwa do");
-=======
+
 		System.out.println("Thik h Bata Bol Denge Tum Tention na lo"); 
 		System.out.println("Anisha ko patwa do"); 
 		System.err.println("Ye line Dekh rhe ho na Kis Color Se Likhi h .......Ab Samjh jao");
->>>>>>> branch 'master' of https://github.com/JackUse943/JavaProject.git
+
 		
 	}
 
