8967cf2e969384a2878ef8b393fa63a62511d415
Abhishek Gupta <abhisheksatna001@gmail.com>
2024-11-27 23:06:40 +0530

    upo

--- JavaProject/src/com/nit/test/Test.java
+++ JavaProject/src/com/nit/test/Test.java
@@ -2,16 +2,14 @@ package com.nit.test;
 
 public class Test {
 
-	public static void main(String[] args)
-	{
-		
+	public static void main(String[] args) {
+
 		System.out.println("Abhishek Class ");
 		System.err.println("Suraj Bhaiya Sonam Bhabhi ko mera navaskar krna");
-		System.out.println("Thik h Bata Bol Denge Tum Tention na lo"); 
-		System.out.println("Anisha ko patwa do"); 
+		System.out.println("Thik h Bata Bol Denge Tum Tention na lo");
+		System.out.println("Anisha ko patwa do");
+		System.out.println("Anisha ko patwa do");
 		
-		
-
 	}
 
 }
