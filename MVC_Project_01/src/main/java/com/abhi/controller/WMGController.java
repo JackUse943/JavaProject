bdead68e2d7a8780acac600ebc2711670ace409c
Abhishek Gupta <abhisheksatna001@gmail.com>
2024-11-28 22:02:59 +0530

    pull kro

--- MVC_Project_01/src/main/java/com/abhi/controller/WMGController.java
+++ MVC_Project_01/src/main/java/com/abhi/controller/WMGController.java
@@ -1,5 +1,13 @@
 package com.abhi.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class WMGController {
+	@RequestMapping("/")
+	public String showmsg() {
+		return "home";
+	}
 
 }
