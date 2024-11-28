454a8f20197db4c7e4fed8467a29472ccc4b0f0d
Abhishek Gupta <abhisheksatna001@gmail.com>
2024-11-28 21:35:06 +0530

    new proj

---
+++ MVC_Project_01/src/main/java/com/abhi/MvcProject01Application.java
@@ -0,0 +1,13 @@
+package com.abhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcProject01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcProject01Application.class, args);
+	}
+
+}
